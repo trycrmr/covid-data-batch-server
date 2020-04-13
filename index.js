f71c8907c4f1e24ab00f1e12c733cc9b355a39c2
trycrmr <trycrmr@gmail.com>
2020-04-13 09:06:06 -0400

    Switches limit from 30 to 28

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const createJHUGlobalTree = require('./createJHUGlobalTree')
 ;(async() => {
 
     const last7Days = await createJHUGlobalTree.create(7)
-    const last28Days = await createJHUGlobalTree.create(30)
+    const last28Days = await createJHUGlobalTree.create(28)
     const allDataToDate = await createJHUGlobalTree.create()
 
     fs.writeFileSync(`${__dirname}/dev/jhu-data-7-days.json`, JSON.stringify(last7Days));
